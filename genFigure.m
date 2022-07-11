5eba5d311a1abbf38ab0d6cb9968f8c1bec114fe
Ahmed Abbasi <ahmed.abbasi@tufts.edu>
2022-07-10 22:23:21 -0400

    commit

--- genFigure.m
+++ genFigure.m
@@ -1,15 +1,17 @@
+close all
+load("dataAltMin.mat")
 n = 1000;
 m = 1;
 d = 100;
 SNR  = 100;
-
-plot(1:length(k_),err_naive,'-x','Color','#D95319',...
+hold on
+plot(1:length(k_),naive_altMIn,'-x','Color','#D95319',...
     'DisplayName','Alt-min mulitple init.',...
     'MarkerSize',11,'Linewidth',1.65);
-plot(1:length(k_),err_stochastic,'-x','Color','#EDB120',...
+plot(1:length(k_),stochastic,'-x','Color','#EDB120',...
     'DisplayName','Stochastic Alt-min',...
     'MarkerSize',11,'Linewidth',1.65);
-plot(1:length(k_),err_proposed,'-x','Color','#7E2F8E',...
+plot(1:length(k_),proposed,'-x','Color','#7E2F8E',...
     'DisplayName','Proposed',...
     'MarkerSize',11,'Linewidth',1.65);
 
@@ -18,12 +20,14 @@ xlabels = k_;
 set(gca, 'XTick', xticks, 'XTickLabel', k_,'Fontsize',14);
 grid('on');
 xlabel('number of shuffles $k$','interpreter','Latex','Fontsize',14);
-ylabel('$|| ||_2^2$','interpreter','Latex','Fontsize',14)
+ylabel('Estimation error $||\mathbf{x}^* - \hat \mathbf x||_2$','interpreter','Latex','Fontsize',14)
 Lgnd =  legend('show');
-%set(Lgnd, 'Interpreter','Latex','Fontsize',12,'Location','Northwest')
-set(Lgnd, 'Interpreter','Latex','Fontsize',12)
+set(Lgnd, 'Interpreter','Latex','Fontsize',12,'Location','Southeast')
+%set(Lgnd, 'Interpreter','Latex','Fontsize',12)
 title(['$ \mathbf P^*_k, \, n = $ ',num2str(n), ' $ m = $ ', num2str(m), ' $ d = $ ', num2str(d),...
         '$\mathbf{B} \sim N(0,1)$'],...
         'interpreter','Latex','Fontsize',16)
 set(gca,'FontSize',16)
 ax = gca;
+exportgraphics(ax,'alt_min.pdf','Resolution',300) 
+saveas(gcf,'alt_min.fig')
