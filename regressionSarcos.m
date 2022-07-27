5c0ca478e9225193c7ef0bb119e796186b1faa38
Ahmed <ahmedabbasi000@gmail.com>
2022-07-26 21:41:14 -0400

    commit

--- regressionSarcos.m
+++ regressionSarcos.m
@@ -1,10 +1,6 @@
 clc
 close all
 clear all
-rng('default')
-addpath(genpath('.\misc'),...
-        genpath('.\benchmarks'),...
-        genpath('.\altMinProposed')); 
 load('sarcos_inv.mat')
 X = sarcos_inv(:,1:21);
 Y = sarcos_inv(:,22:28);
@@ -13,17 +9,15 @@ Y = sarcos_inv(:,22:28);
 %size(Y,1)
 %X = X(~TF,:);
 % make one of the features the block label
-length(unique(round(X(:,7),2))) %~= 2800 blocks
-X(:,7) = round(X(:,7),2);
-blkLabel = X(:,7);
-sf = 3;
-length(unique(blkLabel))
+length(unique(round(X(:,6),3))) %~= 2800 blocks
+X(:,6) = round(X(:,6),3);
+blkLabel = X(:,6);
 %length(unique(X(:,7))) %~= 2800 blocks
 X = X - mean(X,1);
 Y = Y - mean(Y,1);
 [U,S,V] = svd(X,'econ');
 X = U(:,1:10);
-% drop one of the response variable (first column of Y) to improve fit
+% drop one of the response variables (first column of Y) to improve fit
 idx = setdiff(1:size(Y,2),1);
 Y = Y(:,idx);
 % sort blockwise
@@ -53,17 +47,16 @@ R2_naive =  1 - norm(Y-YHat,'fro')^2/norm(Y,'fro')^2
 %----------- proposed --------------------------------------------------
 rLocal = 1;
 lsInit = 0;
-maxIter = 35;
+maxIter = 25;
 tic 
-[pi_hat]     = lp_ls_alt_min_prox(X,Y_permuted,r_,maxIter,rLocal,lsInit);
+[pi_hat]     = AltMin(X,Y_permuted,r_,maxIter,rLocal,lsInit);
 tProposed    = toc;
 Bproposed    = X(pi_hat,:) \ Y_permuted;
+%beta_pro_err = norm(B,2)/norm(beta_star,2);
 R2_pro       = 1 - norm(Y-X*Bproposed,'fro')^2/norm(Y,'fro')^2
 lsInit       = 1;
-[pi_hat]     = lp_ls_alt_min_prox(X,Y_permuted,r_,maxIter,rLocal,lsInit);
+[pi_hat]     = AltMin(X,Y_permuted,r_,maxIter,rLocal,lsInit);
 tProposed    = toc;
-Bproposed     = X(pi_hat,:) \ Y_permuted;
+Bproposed    = X(pi_hat,:) \ Y_permuted;
 R2_proLS     = 1 - norm(Y-X*Bproposed,'fro')^2/norm(Y,'fro')^2
-R2_true
-num_blocks = length(r_)
 
