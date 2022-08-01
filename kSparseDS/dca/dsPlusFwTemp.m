a1f0653df5ca86f14318dc194b095420491447a3
Ahmed <ahmedabbasi000@gmail.com>
2022-07-31 21:49:55 -0400

    commit

--- kSparseDS/dca/dsPlusFwTemp.m
+++ kSparseDS/dca/dsPlusFwTemp.m
@@ -4,10 +4,10 @@ close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % min_P ||Bx - Py ||_2 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-n            = 500;
-d            = 100;
-m            = 50;
-numAssigned  = 250;
+n            = 100;
+d            = 25;
+m            = 10;
+numAssigned  = 40;
 MC           = 1;
 B            = randn(n,d);
 orthB        = B*pinv(B);
@@ -28,15 +28,19 @@ tol = 5e-4;
 FOld = 1e5;
 F = 0;
 P = eye(n);
-%P = ones(n,n)/n;
+P = ones(n,n)/n;
 while(norm(FOld - F) > tol)        
        FOld  = F;
        [S,G] = getDir(orthB,P,Y,Aeq,diagIneq,numAssigned);
        StepS = - trace(orthB*S*(Y*Y')*P')/...
                  trace(orthB*S*(Y*Y')*S');
+       %D = s - P;
+       %ss = -trace( (orthB*P*Y)'*orthB*D*Y)/...
+       %      trace(  orthB*D*Y,'fro')^2;
        if( StepS > 1 || StepS < 0)
-       StepS  = stepSearch(orthB,P,Y,S,G);
+       StepS  = gamma_search(orthB,P,Y,S,G);
        end
+       %P = P + ss*(s-P);
        P = (1-StepS)*P + StepS*S;
        F = norm(orthB*P*Y,'fro')^2
 end
@@ -58,24 +62,23 @@ function [pi_map] = get_permutation(n,num_assigned)
         pi_map(t,idx_p(t)) = 1;
     end
 end
-function [D,G] = getDir(orthB,P,Y,Aeq,diagIneq,numAssigned)
+function [s,G] = getDir(orthB,P,Y,Aeq,diagIneq,numAssigned)
      n = size(P,1);
-     m = size(P,2);
-     G = orthB*P*(Y*Y')/(n*m);
+     G = 2*(orthB'*orthB)*P*(Y*Y');
      options = optimoptions('linprog','Display','none');
      %tic
-     D = linprog(reshape(G,[n^2,1]),...
+     s = linprog(reshape(G,[n^2,1]),...
                  -diagIneq,-numAssigned,...  % trace inequality constraints
                  Aeq,ones(2*n,1),...         % equality row, column
                  zeros(n*n,1),[],options);   % > 0 constraints
      %toc
-     D = reshape(D,[n,n]);
+     s = reshape(s,[n,n]);
 end
-function [gamma] = stepSearch(orthB,P,Y,D,gradient)
+function [gamma] = gamma_search(orthB,P,y,s,gradient)
     gamma         = 0.01;
-    objFunc       = @(P)  norm(orthB*P*Y,'fro')^2;
+    objFunc       = @(P)  norm(orthB*P*y,'fro')^2;
     objFuncValue  = objFunc(P);
-    dir           = D - P;
+    dir           = s-P;
     while (objFunc( P + gamma*dir ) > objFuncValue + 1e-1*gamma*trace(dir'*gradient))
         gamma = 0.75*gamma;
         if (gamma < 1e-5 )
