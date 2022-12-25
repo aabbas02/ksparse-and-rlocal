dc5bda904adcd403cb6d58150197c1bef6bbfc16
Ahmed Abbasi <ahmed.abbasi@tufts.edu>
2022-12-24 20:28:11 -0500

    commit

---
+++ Fig3/fig3a.m
@@ -0,0 +1,113 @@
+clc
+close all;
+clear all
+addpath(genpath('.\misc'),...
+        genpath('.\alt_min'),...
+        genpath('.\benchmarks')); 
+MC              = 1000;
+SNR             = 100;
+d               = 100;
+m               = 50;
+r_              = [125];
+n               = 1000;
+d_H_levsort     = zeros(1,length(r_));
+d_H_sls         = zeros(1,length(r_));
+d_H_one_step    = zeros(1,length(r_));
+d_H_rlus        = zeros(1,length(r_));
+d_H_biconvex    = zeros(1,length(r_));
+d_H_alt_min     = zeros(1,length(r_));
+rho_            = -3:1:1;
+rho_            = 10.^rho_;
+rLocal          = 1;
+lsInit          = 0;
+T = 0;
+maxIter         = 25;
+for j = 1 : length(r_)
+	r = r_(j);
+    r_arr = ones(1,n/r)*r;
+    for k = 1 : MC
+                B                = randn(n,d);
+                X                = randn(d,m);
+                Y                = B*X;  
+                noise_var   	 = 1*norm(X,'fro')^2  / (SNR*m);
+                W                = sqrt(noise_var)*randn(n,m);
+                pi_              = get_permutation_r(n,r_arr); 
+                %pi_              = get_permutation_k(n,n/5);
+                Y_permuted       = Y(pi_,:);
+                Y_permuted_noisy = Y_permuted + W;
+                %---rlus  https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=9440727 
+                %t1_start = tic;
+%                 tic
+%                 pi_rlus          = rlus(B,Y_permuted_noisy,r_arr,rLocal);
+%                 toc
+                %toc(t1_start)
+%                 d_H              = sum(pi_ ~= pi_rlus)/n;
+%                 d_H_rlus(j)      = d_H + d_H_rlus(1,j);
+                %---biconvex https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8849447
+%  				d_H_min = 1;
+%                 tic
+%                 for i = 1 : length(rho_) % cross validate across rho paramter
+%                    rho              = rho_(i);
+%                    pi_admm          = admm(B,Y_permuted_noisy,r_arr,rho);
+%                    d_H_             = sum(pi_ ~= pi_admm)/n;
+%                    if d_H_ < d_H_min
+%                       d_H_min = d_H_;
+%                    end
+%                 end
+%                 toc
+%                 d_H_biconvex(j) = d_H_biconvex(j) + d_H_min;
+                %---icml https://proceedings.mlr.press/v119/zhang20n.html
+%                 pi_icml            = icml_20(B,Y_permuted_noisy,r_arr);
+%                 d_H_one_step(j)    = d_H_one_step(j) + sum(pi_ ~= pi_icml)/n;   
+                %---slawski 
+%                 pi_sls             = slawski(B,Y_permuted_noisy,noise_var,r_arr);
+%                 d_H_sls(j)         = d_H_sls(j) + sum(pi_ ~= pi_sls)/n;                   
+%               %---levsort  https://people.eecs.berkeley.edu/~courtade/pdfs/DenoisingLinearModels_ISIT2017.pdf
+%                 pi_lev             = levsort(B,Y_permuted_noisy,r_arr);
+%                 d_H_levsort(j)     = d_H_levsort(j) + sum(pi_ ~= pi_lev)/n;                   
+                %---alt-min/proposed
+                tic
+                [pi_alt_min]       = AltMin(B,Y_permuted_noisy,r_arr,maxIter,rLocal,lsInit);
+                d_H                = sum(pi_ ~= pi_alt_min)/n;
+                d_H_alt_min(j)     = d_H + d_H_alt_min(j); 
+                T = toc + T;
+    end
+    j
+end
+T/MC
+d_H_alt_min      = d_H_alt_min/MC;
+d_H_one_step     = d_H_one_step/MC;
+d_H_rlus         = d_H_rlus/MC;
+d_H_levsort      = d_H_levsort/MC;
+d_H_sls          = d_H_sls/MC;
+d_H_biconvex     = d_H_biconvex/MC;
+hold on;
+plot(1:length(r_),d_H_one_step,'-x','Color','#0072BD',...
+    'DisplayName','Spectral',...
+    'MarkerSize',11,'Linewidth',1.65);
+plot(1:length(r_),d_H_biconvex,'-x','Color','#77AC30',...
+     'DisplayName','Biconvex',...
+     'MarkerSize',11,'Linewidth',1.65);
+plot(1:length(r_),d_H_rlus,'-x','Color','#D95319',...
+    'DisplayName','RLUS',...
+    'MarkerSize',11,'Linewidth',1.65);
+plot(1:length(r_),d_H_sls,'-x','Color','#EDB120',...
+    'DisplayName','$\ell_2$-regularized',...
+    'MarkerSize',11,'Linewidth',1.65);
+plot(1:length(r_),d_H_alt_min,'-x','Color','#7E2F8E',...
+    'DisplayName','Proposed',...
+    'MarkerSize',11,'Linewidth',1.65);
+xticks = 1:length(r_);
+set(gca, 'XTick', xticks, 'XTickLabel', r_,'Fontsize',14);
+grid('on');
+xlabel('block size $r$','interpreter','Latex','Fontsize',14);
+ylabel('$d_H/n$','interpreter','Latex','Fontsize',14)
+Lgnd =  legend('show');
+set(Lgnd, 'Interpreter','Latex','Fontsize',12,'Location','Southeast')
+title(['$ \mathbf P^*_r \, n = $ ',num2str(n), ' $ m = $ ', num2str(m), ' $ d = $ ', num2str(d),...
+        ' $\mathbf{B} \sim N(0,1)$'],...
+        'interpreter','Latex','Fontsize',16)
+set(gca,'FontSize',16)
+ax = gca;
+%exportgraphics(ax,'r_local.pdf','Resolution',300) 
+%saveas(gcf,'r_local.fig')
---
+++ Table1/regressionSCM.m
@@ -0,0 +1,95 @@
+% Dataset download URL:
+% Description: http://proceedings.mlr.press/v115/slawski20a/slawski20a.pdf
+% Preprocessing and dataset description: http://auai.org/uai2019/proceedings/papers/7.pdf
+rng("default")
+clc
+close all
+clear all
+rng('default')
+dir = pwd;
+% For linux, replace '\' with '/'
+idcs   = strfind(dir,'\');
+newdir = dir(1:idcs(end)-1);
+cd (newdir)
+addpath(genpath('.\misc'),...
+        genpath('.\benchmarks'),...
+        genpath('.\altMinProposed'),...
+        genpath('.\dataSets'))
+A = readmatrix('scm.csv');
+Y = A(:,63:end);
+X = A(:,2:62);
+% zero-mean the columns
+X = X - mean(X,1);
+Y = Y - mean(Y,1);
+% make one of the features the block label and round that feature to sf
+% significant figures
+sf = 1;
+numBlocks = length(unique(round(X(:,7),sf))); %~= 2800 blocks
+blkLabel = round(X(:,7),sf);
+[blkLabelSorted,idx] = sort(blkLabel);
+% order blockwise
+Y = Y(idx,:);
+X = X(idx,:);
+% get lengths of blocks
+temp = unique(blkLabelSorted);
+r_ = zeros(1,length(temp));
+for i = 1:length(temp)
+    t1 = find(blkLabelSorted == temp(i),1,'first');
+    t2 = find(blkLabelSorted == temp(i),1,'last');
+    r_(i) = t2-t1+1;
+end
+[U,S,V] = svd(X,'econ');
+% retain top d = 35 principal components
+X = U(:,1:35);
+n   = size(Y,1);
+pi_ = get_permutation_r(n,r_);
+rLocal = 1;
+Y_permuted = Y(pi_,:);
+%---------------- oracle -----------------------------------
+Btrue = X \ Y;
+R2_true  = 1 - norm(Y-X*Btrue,'fro')^2/norm(Y - mean(Y,1),'fro')^2;
+%---------------- naive ------------------------------------
+Bnaive = X \ Y_permuted;
+R2_naive  = 1 - norm(Y-X*Bnaive,'fro')^2/norm(Y,'fro')^2;
+beta_naive_err = norm(Bnaive - Btrue,2)/norm(Btrue,2);
+%---------------- proposed ----------------------------------
+maxIter = 25;
+lsInit = 0;
+%---------------- w collapsed init --------------------------
+[pi_hat,fVal] = AltMin(X,Y_permuted,r_,maxIter,rLocal,lsInit);
+Bpro     = X(pi_hat,:) \ Y_permuted;
+BproErr  = norm(Bpro - Btrue,2)/norm(Btrue,2);
+R2_pro   = 1 - norm(Y-X*Bpro,'fro')^2/norm(Y,'fro')^2;
+%--------------- w least-squares init -----------------------
+lsInit       = 1;
+[pi_hat,fValLS]   = AltMin(X,Y_permuted,r_,maxIter,rLocal,lsInit);
+Bpro     = X(pi_hat,:) \ Y_permuted;
+R2_proLS     = 1 - norm(Y-X*Bpro,'fro')^2/norm(Y,'fro')^2;
+BproLSerr = norm(Bpro - Btrue,2)/norm(Btrue,2);
+%------------------ slawski ---------------------------------
+noise_var    = norm(Y_permuted-X*Bnaive,'fro')^2/(size(Y,1)*size(Y,2));
+tic
+[pi_hat,~]   = slawski(X,Y_permuted,noise_var,r_);
+tSLS         = toc;
+beta_sls     = X(pi_hat,:)\Y_permuted;
+BslsErr      = norm(beta_sls - Btrue,2)/norm(Btrue,2); 
+R2_sls       = 1 - norm(Y-X*beta_sls,'fro')^2/norm(Y,'fro')^2;
+%----------------- RLUS ---------------------------------------
+tic
+[pi_hat]    = rlus(X,Y_permuted,r_,rLocal);
+beta_RLUS   = X(pi_hat,:) \ Y_permuted;
+R2_rlus     = 1 - norm(Y-X*beta_RLUS,'fro')^2/norm(Y,'fro')^2;
+BrlusErr    = norm(beta_RLUS - Btrue,2)/norm(Btrue,2); 
+tRlus = toc;
+%---------------------------------------------------------------
+R2_true 
+R2_naive
+R2_pro
+R2_proLS
+R2_sls
+R2_rlus
+beta_naive_err
+BproErr
+BproLSerr
+BslsErr
+BrlusErr
