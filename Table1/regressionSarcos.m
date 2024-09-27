2b4a768584cc10aed0749c200a01ea02120b42ee
Abbasi <aabbasi1@iastate.edu>
2024-09-27 14:44:23 -0500

    commit

--- Table1/regressionSarcos.m
+++ Table1/regressionSarcos.m
@@ -62,42 +62,46 @@ maxIter = 25;
 lsInit = 0;
 % %---------- w collapsed init --------------------------
 % % tic
+tStart = tic;
 [pi_hat,fVal] = AltMin(X,Y_permuted,r_,maxIter,rLocal,lsInit);
 %tAltMin = toc
 Bpro    = X(pi_hat,:) \ Y_permuted;
 beta_pro_err = norm(Bpro - Btrue,2)/norm(Btrue,2);
 R2_pro       = 1 - norm(Y-X*Bpro,'fro')^2/norm(Y,'fro')^2;
+tAltMinCllpsd = toc(tStart);
 % % %---------- w least-squares init -----------------------
+tStart = tic;
 lsInit       = 1;
 [pi_hat,fValLS]   = AltMin(X,Y_permuted,r_,maxIter,rLocal,lsInit);
 Bpro         = X(pi_hat,:) \ Y_permuted;
 R2_proLS     = 1 - norm(Y-X*Bpro,'fro')^2/norm(Y,'fro')^2;
 BproLSerr = norm(Bpro - Btrue,2)/norm(Btrue,2);
+tAltMinLSInit = toc(tStart);
 %------------------ slawski ---------------------------------
-noise_var    = norm(Y_permuted-X*Bnaive,'fro')^2/(size(Y,1)*size(Y,2));
-tic
-[pi_hat,~]   = slawski(X,(Y_permuted),noise_var,r_);
-tSLS         = toc;
-beta_sls     = X(pi_hat,:)\Y_permuted;
-beta_sls_err = norm(beta_sls - Btrue,2)/norm(Btrue,2); 
-R2_sls       = 1 - norm(Y-X*beta_sls,'fro')^2/norm(Y,'fro')^2;
+%noise_var    = norm(Y_permuted-X*Bnaive,'fro')^2/(size(Y,1)*size(Y,2));
+%tic
+%[pi_hat,~]   = slawski(X,(Y_permuted),noise_var,r_);
+%tSLS         = toc;
+%beta_sls     = X(pi_hat,:)\Y_permuted;
+%beta_sls_err = norm(beta_sls - Btrue,2)/norm(Btrue,2); 
+%R2_sls       = 1 - norm(Y-X*beta_sls,'fro')^2/norm(Y,'fro')^2;
 %----------------- RLUS ---------------------------------------
-tic
-[pi_hat] = rlus(X,Y_permuted,r_,rLocal);
-beta_RLUS = X(pi_hat,:) \ Y_permuted;
-R2_rlus  = 1 - norm(Y-X*beta_RLUS,'fro')^2/norm(Y,'fro')^2;
-tRlus = toc;
-beta_rlus_err = norm(beta_RLUS - Btrue,2)/norm(Btrue,2);
+%tic
+%[pi_hat] = rlus(X,Y_permuted,r_,rLocal);
+%beta_RLUS = X(pi_hat,:) \ Y_permuted;
+%R2_rlus  = 1 - norm(Y-X*beta_RLUS,'fro')^2/norm(Y,'fro')^2;
+%tRlus = toc;
+%beta_rlus_err = norm(beta_RLUS - Btrue,2)/norm(Btrue,2);
 %----------------------------------------------------------------
 %---------------- altGDMin  w rLocal Init--------------------------------------
-tic
+tStart = tic;
 lsInit = 0;
 maxIter = 100;
 [pi_hat,fvalAltGDMin] = altGDMin(X,Y_permuted,r_,maxIter,rLocal,lsInit);
 beta_altGDMin = X(pi_hat,:) \ Y_permuted;
 R2_altGDMin  = 1 - norm(Y-X*beta_altGDMin,'fro')^2/norm(Y,'fro')^2;
 beta_altGDMin_err = norm(beta_altGDMin - Btrue,2)/norm(Btrue,2);
-taltGDmin = toc;
+taltGDmin = toc(tStart);
 %--------------------------------------------------------------------------
 numBlocks
 R2_true 
